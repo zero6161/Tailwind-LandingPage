3c42a92a822d83945ebe830fd1e36737e13bbfdb
zero6161 <111440977+zero6161@users.noreply.github.com>
2023-03-07 14:43:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Content from "./components/Content/Content";
-import NavBar from "../src/components/Navbar/Navbar";
 import Slogan from "./components/Slogan/Slogan";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  const [isMobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    const handleSize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        heigth: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleSize);
+    handleSize();
+    return () => window.removeEventListener("resize", handleSize);
+  }, []);
+
+  useEffect(() => {
+    if (windowSize.width < 500) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }, [windowSize]);
   return (
-    <div className="main-container">
-      <NavBar />
+    <div className="font-sora overflow-y-auto overflow-x-hidden h-screen px-4 py-8 bg-gradient-to-b dark:from-purple-900 dark:to-purple-700 from-white to-pink-500  dark:text-white text-black md:px-20">
+      <Navbar isMobile={isMobile} />
       <Slogan />
       <Content />
     </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { BsSunFill } from "react-icons/bs";
 import { HiOutlineMenu } from "react-icons/hi";
 import { MdOutlineClose } from "react-icons/md";
-const Navbar = () => {
+const Navbar = (props) => {
+  const { isMobile } = props;
   const [openMenu, setOpenMenu] = useState(false);
   const handleMenu = () => {
     setOpenMenu(!openMenu);
@@ -13,19 +14,26 @@ const Navbar = () => {
         <div className="text-20 font-bold mr-2">NerdCard</div>
         <BsSunFill size={"24px"} color="#e9c46a" className="cursor-pointer" />
       </div>
-      <ul className="ml-auto text-16 font-semibold">
-        {openMenu ? (
+      <ul className="md:flex md:gap-10 ml-auto text-16 font-semibold">
+        {openMenu && isMobile ? (
           <MdOutlineClose
             size="21px"
             className="cursor-pointer"
             onClick={handleMenu}
           />
-        ) : (
+        ) : !openMenu && isMobile ? (
           <HiOutlineMenu
             size={"24px"}
             className="cursor-pointer"
             onClick={handleMenu}
           />
+        ) : (
+          <>
+            <li className="btn-hover">Features</li>
+            <li className="btn-hover">Menu</li>
+            <li className="btn-hover">Our Story</li>
+            <li className="btn-hover ml-28">Contact</li>
+          </>
         )}
         {openMenu && (
           <div className="absolute right-8 bg-white p-8 text-black text-center text-13">
